a4309e3972770e13ebca3f5e4965ed777e8c37db
dooroojoo <dooroojoo@kakao.com>
2021-11-02 20:32:02 +0900

    카카오/이메일 로그인 수정

--- Semi_Project/src/com/soda/member/controller/EmailLoginServlet.java
+++ Semi_Project/src/com/soda/member/controller/EmailLoginServlet.java
@@ -8,22 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.soda.member.model.service.MemberService;
-import com.soda.member.model.vo.Member;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class EmailLoginServlet
  */
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/email/login")
+public class EmailLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginServlet() {
+    public EmailLoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,7 +28,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/member/loginPage.jsp");
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/member/emailLoginPage.jsp");
 		view.forward(request, response);
 	}
 
@@ -40,21 +36,8 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String userId = request.getParameter("email");
-		String userPwd = request.getParameter("pwd");
-		
-		Member loginUser = new MemberService().loginMember(userId, userPwd); 
-		
-		if(loginUser != null) {
-			HttpSession session = request.getSession();
-			session.setAttribute("loginUser", loginUser);
-			response.sendRedirect(request.getContextPath()+ "/mainpage");
-		} else {
-			/* ajax로 로그인 실패 시 로그인폼 하단에 텍스트 나타내기 */ 
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().print("로그인 정보가 일치하지 않습니다.");
-		}
-		
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
